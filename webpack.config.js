7abf12f66f23e29f99ebb5f70515597e7e620fb6
matthewjohnmiller2020 <matthewjohnmiller2020@gmail.com>
2022-03-12 12:20:53 -0500

    Built webpack

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html", 
+  filename: "./index.html"
+});
+module.exports = {
+  mode: 'development',
+  entry: "./src/index.js",
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "src"),
+      publicPath: 'main.js',
+    },
+    port: 8080,
+    proxy: {
+      '/api': 'http:localhost:3000'
+    },
+  },
+  plugins: [htmlPlugin],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.s?css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  }
+};
