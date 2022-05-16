b9849a27e15f57eca0297d7b42c976734af84225
matthewjohnmiller2020 <matthewjohnmiller2020@gmail.com>
2022-05-16 19:18:46 -0400

    modified second argument in useEffect

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -3,10 +3,9 @@ import { useEffect } from 'react/cjs/react.production.min';
 import Habit from './Habit.jsx';
 
 const Dashboard = () => {
-  const [refetch, setRefetch] = useState(true);
   const [habits, setHabits] = useState([]);
-  useEffect(() => {
-    if(refetch){
+  useEffect(() =>
+  {
       fetch('/habits/getHabits', {
         method: "POST",
         headers: {"Content-Type": "application/json; charset=UTF-8"},
@@ -15,21 +14,19 @@ const Dashboard = () => {
       .then((data) => data.json())
       .then((data) => {
         const habitCards = [];
-      for(let habit of data.habits){
-        habitCards.push(
-          <Habit 
-          key={habit.habitid}
-          habitname={habit.habitname}
-          moneyspent={habit.moneyspent}
-          // lasttime={habit.lasttime} 
-          />
+        for(let habit of data.habits){
+          habitCards.push(
+            <Habit 
+              key={habit.habitid}
+              habitname={habit.habitname}
+              moneyspent={habit.moneyspent}
+              // lasttime={habit.lasttime}
+               />
         )
       }
       setHabits(habitCards);
     })
-    .finally(() => setRefetch(false))
-    }
-  }, [refetch])
+  }, [])
   return (
     <div className='dash'>
       <h1>Your habits:</h1>
